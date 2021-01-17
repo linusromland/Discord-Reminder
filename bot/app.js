38cb326f3d2533ac73969b5d9383e10e21a9659e
Linus Romland <romlandlinus@gmail.com>
2021-01-17 12:36:16 +0100

    added discordjs for the bot

--- bot/app.js
+++ bot/app.js
@@ -1,5 +1,14 @@
 const schedule = require('node-schedule');
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+bot.on("ready", () => {
+    bot.user.setGame('Checking you Schedule');
+    console.log(`Bot is online!\n${bot.users.size} users, in ${bot.guilds.size} servers connected.`);
+});
 
 schedule.scheduleJob({hour: 12, minute: 32}, () => {
     console.log('cool');
 });
+
+bot.login(config.token);
